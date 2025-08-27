1d8e3cfe68b46b8cedfb903d1815b5968cb89c26
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2025-08-27 00:22:08 -0300

    test [red]: número único lança NotImplemented

--- StringCalculator.Test/Tests.cs
+++ StringCalculator.Test/Tests.cs
@@ -11,4 +11,12 @@ public class Tests
         var result = calculator.Sum("");
         result.Should().Be(0);
     }
+
+    [Fact]
+    public void Given_SingleNumberString_When_Sum_Then_ReturnsThatNumber()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Sum("1");
+        result.Should().Be(1);
+    }
 }
