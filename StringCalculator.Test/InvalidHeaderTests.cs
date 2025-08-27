7fb62ea6fa475cc4b4f30f0307f8ad7574d7daa1
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2025-08-27 11:15:07 -0300

    test [green]: adiciona testes de robustez para headers inválidos

---
+++ StringCalculator.Test/InvalidHeaderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculator.Test;
+
+public class InvalidHeaderTests
+{
+    [Fact]
+    public void When_HeaderHasNoNewline_Then_ShouldThrow()
+    {
+        var calc = new StringCalculator();
+        Assert.Throws<FormatException>(() => calc.Sum("//[***]1***2"));
+    }
+
+    [Fact]
+    public void When_HeaderHasUnclosedBracket_Then_ShouldThrow()
+    {
+        var calc = new StringCalculator();
+        Assert.Throws<FormatException>(() => calc.Sum("//[***\n1***2"));
+    }
+
+    [Fact]
+    public void When_HeaderHasEmptyDelimiter_Then_ShouldThrow()
+    {
+        var calc = new StringCalculator();
+        Assert.Throws<FormatException>(() => calc.Sum("//[]\n1"));
+    }
+
+    [Fact]
+    public void When_HeaderHasSingleWithoutBracketsButMoreThanOneChar_Then_ShouldThrow()
+    {
+        var calc = new StringCalculator();
+        Assert.Throws<FormatException>(() => calc.Sum("//ab\n1ab2"));
+    }
+
+    [Fact]
+    public void When_HeaderHasEmptyAndNonEmptyDelimiters_Then_ShouldThrow()
+    {
+        var calc = new StringCalculator();
+        Assert.Throws<FormatException>(() => calc.Sum("//[][;]\n1;2"));
+    }
+}
