4fb36530125126965b46a42a0cbb1052f44a1df5
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2025-08-27 00:23:16 -0300

    feat [green]: Sum retorna o próprio número para entrada única

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -5,6 +5,9 @@ public class StringCalculator
     public int Sum(string input)
     {
         if (string.IsNullOrEmpty(input)) return 0;
+        
+        return int.Parse(input);
+        
         throw new NotImplementedException();
     }
 }
