fc9c363cb508ed12bcd63755d211a94004f66c1d
Thales Vieira Pereira <56850814+Thalesvpr@users.noreply.github.com>
2025-08-27 00:07:32 -0300

    test [red]: StringCalculator lança NotImplemented para string vazia

---
+++ StringCalculator.Test/Tests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator.Test;
+
+public class Tests
+{
+    [Fact]
+    public void Given_EmptyString_When_Sum_Then_ReturnsZero()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Sum("");
+        result.Should().Be(0);
+    }
+}
